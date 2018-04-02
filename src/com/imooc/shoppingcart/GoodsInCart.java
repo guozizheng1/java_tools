5732d97e399a316296770684cd226ae7a91bb312
gzzwinner <30675995+gzzwinner@users.noreply.github.com>
2018-04-02 14:09:25 +0800

    购物车详细设计 提交

--- src/com/imooc/shoppingcart/GoodsInCart.java
+++ src/com/imooc/shoppingcart/GoodsInCart.java
@@ -5,5 +5,32 @@
 package com.imooc.shoppingcart;
 
 public class GoodsInCart {
+	private Goods goods;
+	private int num;
+	
+	//构造方法
+	public GoodsInCart(Goods goods, int num) {
+		this.goods = goods;
+		this.num = num;
+	}
+
+	//getter和setter方法
+	public Goods getGoods() {
+		return goods;
+	}
+
+	public void setGoods(Goods goods) {
+		this.goods = goods;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
 
 }
