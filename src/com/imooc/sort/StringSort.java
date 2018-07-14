b000d7c1e8e0fe6e2fd917fca834195731d0d1a4
Administrator <gzzwinner@163.com>
2018-07-14 09:25:46 +0800

    StringSort

---
+++ src/com/imooc/sort/StringSort.java
@@ -0,0 +1,34 @@
+package com.imooc.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StringSort {
+    public static void main(String args[]){
+        //给list添加元素
+        List<String> list = new ArrayList<String>();
+        list.add("orange");
+        list.add("tomato");
+        list.add("apple");
+        list.add("litchi");
+        list.add("banana");
+
+        //输出排序前list中的内容
+        System.out.println("排序前：");
+        for(String m: list){
+            System.out.print(m + " ");
+        }
+        System.out.println();
+
+        //对list中的元素进行排序
+        Collections.sort(list);
+
+        //输出排序后list中的内容
+        System.out.println("排序后：");
+        for (String m: list){
+            System.out.print(m + " ");
+        }
+
+    }
+}
