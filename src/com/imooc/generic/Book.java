256392b392c9ce1d620edfcb0eb27af04a895d8f
gzzwinner <30675995+gzzwinner@users.noreply.github.com>
2018-11-29 16:49:44 +0800

    泛型 提交

---
+++ src/com/imooc/generic/Book.java
@@ -0,0 +1,9 @@
+package com.imooc.generic;
+
+
+public class Book extends Goods {
+    @Override
+    public void sell() {
+        System.out.println("sell books");
+    }
+}
---
+++ src/com/imooc/generic/Shoes.java
@@ -0,0 +1,8 @@
+package com.imooc.generic;
+
+public class Shoes extends Goods {
+    @Override
+    public void sell() {
+        System.out.println("sell shoes");
+    }
+}
